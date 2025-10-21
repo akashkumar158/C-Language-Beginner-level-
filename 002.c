c04f31c63884ed1f424ed88214756a728e8485bb
akashkumar158 <akashkumarnath158@gmail.com>
2025-10-21 18:49:15 +0530

    Add files via upload

---
+++ 002.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+        int min,hours;
+        printf("enter minutes");
+        scanf("%d",&min);
+
+        hours=min/60;
+
+        printf("enter h=%d",hours);
+
+}
