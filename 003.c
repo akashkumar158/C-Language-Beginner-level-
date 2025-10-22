6fab9afd51c7c872186ae2b38bb5865b6cf8c8e8
akashkumar158 <akashkumarnath158@gmail.com>
2025-10-22 08:50:34 +0530

    Add files via upload

---
+++ 003.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+        int doll;
+        float p;
+
+        printf("enter dollars");
+        scanf("%d",&doll);
+
+        p= doll/0.75;
+
+        printf("total p are =  %f", p);
+
+}
