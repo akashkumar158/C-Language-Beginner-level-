4af7df1793044c0c616cc69ffd9125f05e4b1fc9
akashkumar158 <akashkumarnath158@gmail.com>
2025-10-22 20:16:57 +0530

    Add files via upload

---
+++ 004.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+   int r;
+   float area,pi=3.14;
+
+   printf("enter radius");
+   scanf("%d",&r);
+
+   area= pi*r*r;
+
+   printf("final A = %f",area);
+
+
+
+
+
+}
