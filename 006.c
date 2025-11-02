027945856305917f3c5787431898e364d04134fa
akashkumar158 <akashkumarnath158@gmail.com>
2025-11-02 06:34:30 +0530

    Add files via upload

---
+++ 006.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+  void main()
+  {
+    int b,m;
+    float tb,t;
+
+    printf("enter members");
+    scanf ("%d",&m);
+
+     b= m*500;
+
+     printf (" \n the basic bill is= %d",b);
+
+     t=b*28/100;
+
+     tb =b+t;
+
+
+     printf("\n\n the bill along with tax included is= %f",tb);
+
+
+
+
+
+  }
