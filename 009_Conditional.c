50c85ccddf0761ca70ecbc90a5f1b242af38a91d
akashkumar158 <akashkumarnath158@gmail.com>
2025-11-06 21:06:17 +0530

    Add files via upload

---
+++ 009_Conditional.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+
+    printf(" Enter value in n :");
+    scanf("%d",&n);
+
+    if(n>0)
+        printf(" Positive value ");
+    else
+        printf(" Negative value ");
+
+
+    printf("\n\n\n\n");
+
+}
