69f0ef9d2aca074fac24e35e51ca7c30ffe9863c
akashkumar158 <akashkumarnath158@gmail.com>
2025-11-08 20:54:29 +0530

    Add files via upload

---
+++ 011.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+ int a,b;
+
+  printf("enter value of A");
+  scanf("%d",&a);
+
+
+  printf("enter value of B");
+  scanf("%d",&b);
+
+  if (a==b)
+
+        printf("vlaues of both A and B are same");
+
+
+   else
+        printf("values of both A and B are not same");
+
+}
