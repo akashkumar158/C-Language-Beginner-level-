e9bfd68b823c7092c53133c04ccaf61e8a94b0b0
akashkumar158 <akashkumarnath158@gmail.com>
2025-11-08 21:27:59 +0530

    Add files via upload

---
+++ 012.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+   int n;
+
+   printf("enter the value of the number");
+   scanf("%d",&n);
+
+   if (n%2 == 0)
+
+       printf("the given number is even");
+
+
+    else
+
+        printf("the given number is odd");
+
+
+
+
+
+
+
+
+
+
+
+
+}
