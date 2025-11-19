5c0e55e5fb7ee72897ec730b094d18d66270933b
akashkumar158 <akashkumarnath158@gmail.com>
2025-11-19 18:07:50 +0530

    Add files via upload

---
+++ 027( 1  3  5  7  9 ).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    i=1;
+
+    while (i<=9)
+    {
+     printf ("%d",i);
+     i= i+2;
+    }
+}
---
+++ 028 ( 9  7  5  3  1).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int i;
+    i=9;
+
+    while (i>=1)
+    {
+        printf ("%d",i);
+        i=i-2;
+    }
+}
