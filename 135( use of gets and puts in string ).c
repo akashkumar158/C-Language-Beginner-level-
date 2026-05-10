73c29e75cd3af70b9f2f59a6e329ba5f98bd159c
akashkumar158 <akashkumarnath158@gmail.com>
2026-05-10 09:46:22 +0530

    Add files via upload

---
+++ 135( use of gets and puts in string ).c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+
+ void main()
+ {
+    int i;
+
+    char name[100];
+
+    printf (" enter a sentence ");
+    gets(name);
+
+    printf("the sentence is =");
+    puts(name);
+
+
+ }
